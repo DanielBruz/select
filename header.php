e98540e3751b4135909dafc2f25e0f234f398ba9
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-06-22 14:24:02 +0200

    First commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>fun times php</title>
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body class="container">
+
+	<div class="page-header">
+		<h1 class="text-muted">fun times php</h1>
+
+	</div>
+</body>
+</html>  
